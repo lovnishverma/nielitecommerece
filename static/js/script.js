828cf008c965472b08fc2523725747917d244df2
Glitch (nielitecom) <none>
2025-02-22 13:09:30 +0000

    🛎🐚 Checkpoint ./templates/user_orders.html:66950673/1673 ./templates/profileHome.html:66950673/9 ./templates/login.html:66950673/2842 ./templates/register.html:66950673/6008 ./templates/home.html:66950673/4706 ./static/js/script.js:66950673/112 ./static/css/styles.css:66950673/11371

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,7 @@ $('#return-to-top').click(function() {      // When arrow is clicked
         scrollTop : 0                       // Scroll to top of body
     }, 500);
 });
+
+function toggleMenu() {
+            document.querySelector(".nav-links").classList.toggle("active");
+        }
